f3370a49370e7b2b1a3f827d37de24a5b66b8ae7
Vishalmethri <vishalmethri14@gmail.com>
2023-03-12 23:14:26 +0530

    [Add]: Upto Sequelize Complete

--- controllers/shop.js
+++ controllers/shop.js
@@ -127,26 +127,40 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user.getCart()
   .then(cart => {
+    fetchedCart = cart;
     return cart.getProducts();
   })
   .then(products => {
-    console.log(products);
+    return req.user
+    .createOrder()
+    .then(order => {
+      order.addProducts(products.map(product => {
+        product.orderItem = { quantity: product.cartItem.quantity };
+        return product;
+      }));
+    })
+    .catch(err => console.log(err));
+  })
+  .then(result => {
+    return fetchedCart.setProducts(null);
+  })
+  .then(result => {
+    res.redirect('/orders');
   })
   .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    path: "/checkout",
-    pageTitle: "Checkout",
-  });
+  req.user.getOrders({include: ['products']})
+  .then(orders => {
+    res.render("shop/orders", {
+      path: "/orders",
+      pageTitle: "Your Orders",
+      orders: orders
+    });
+  })
+  .catch(err => console.log(err));
 };
